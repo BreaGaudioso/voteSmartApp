def93a7ca653e567d3ca12374d9261538e761476
Brea Gaudioso <breagaudioso@gmail.com>
2015-09-28 13:30:52 -0700

    launch

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/votesmartapp');
-mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/votesmartapp")
+mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/votesmartapp");
 module.exports.Article = require('./article');
 module.exports.Candidate = require('./candidate');
 module.exports.User = require('./user');
